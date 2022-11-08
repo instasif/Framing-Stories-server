29766fad74d4b66a2189bf9a5a36233b74f9aa32
instasif <asifh9277@gmail.com>
2022-11-09 00:00:32 +0600

    goten services data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,9 +10,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2lbo3hl.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run(){
     try{
+        const serveiceCollection = client.db("framingStories").collection("services");
 
+        app.get('/services', async(req, res) =>{
+            const query = {};
+            const cursor = serveiceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
     }
     finally{
 
